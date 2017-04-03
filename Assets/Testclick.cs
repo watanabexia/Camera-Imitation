de99205f7ba21412b0f152c49dc50de6666d5afb
Tongguang Watanabe Xia <ghbq674521@163.com>
2017-04-03 23:57:06 +0800

    2017.4.3

---
+++ Assets/Testclick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testclick : MonoBehaviour
+{
+    public bool is_c = true;
+    public void Click()
+    {
+        if (is_c)
+        {
+            for (int x = 0; x <= 100; ++x)
+            {
+                for (int z = 0; z <= 100; ++z)
+                {
+                    int namegiv = 100000000 + 10000 * x + z;
+                    GameObject clone = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                    clone.transform.localScale = new Vector3(1f, 1f, 1f);
+                    clone.transform.position = new Vector3(1f * x, 0, 1f * z);
+                    clone.name = namegiv + "";
+                }
+            }
+            is_c = false;
+        }
+        
+    }
+}
---
+++ Assets/createcamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createcamera : MonoBehaviour
+{
+    void Update()
+    {
+        // 以摄像机所在位置为起点，创建一条向下发射的射线  
+        Ray ray = Camera
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            // 如果射线与平面碰撞，打印碰撞物体信息  
+            Debug.Log("碰撞对象: " + hit.collider.name);
+            // 在场景视图中绘制射线  
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+        }
+    }
+}
